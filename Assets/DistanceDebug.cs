e36d05f48faa9fcebfe12f107f649b6d69f91208
Kevin Hunter <vudugrv@hotmail.com>
2020-03-27 20:21:43 -0700

    Initial commit

---
+++ Assets/DistanceDebug.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceDebug : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(transform.position, transform.position + (transform.forward * 20));
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(transform.position, transform.position + (transform.forward * 12));
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + (transform.forward * 6));
+    }
+}
